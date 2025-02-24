a411283f768428f004b8f272d0772fdd16bd44e1
Joyce Vagdevi <joycevadgevi2003@gmail.com>
2025-02-24 20:01:54 +0530

    Operators

---
+++ Operators/Bitwise.py
@@ -0,0 +1,36 @@
+#bitwise operators are used to perform bitwise calculations on integers. The integers are first converted into binary and then operations are performed on each bit or corresponding pair of bits, hence the name bitwise operators. 
+
+#Bitwise AND 
+a = 10
+b = 4
+print("a & b =", a & b)
+
+#Bitwise OR
+a = 10
+b = 4
+print("a | b =", a | b)
+
+#Bitwise XOR
+a = 10
+b = 4
+print("a ^ b =", a ^ b)
+
+#Bitwise NOT
+a = 10
+b = 4
+print("~a =", ~a)
+
+#Bitwise right shift 
+a = 10
+b = -10
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+#Bitwise left shift
+a = 5
+b = -10
+print("a << 1 =", a << 1)
+print("b << 1 =", b << 1)
+
+
+
---
+++ Operators/arithmetic.py
@@ -0,0 +1,59 @@
+#arithmetic operators are of the following:
+#ADDITION operator 
+val1 = 2
+val2 = 3
+
+# using the addition operator
+res = val1 + val2
+print(res,"\n")
+
+#SUBTRACTION operator
+val1 = 2
+val2 = 3
+
+# using the subtraction operator
+res = val1 - val2
+print(res,"\n")
+
+#MULTIPLICATION operator
+val1 = 2
+val2 = 3
+
+# using the multiplication operator
+res = val1 * val2
+print(res,"\n")
+
+#DIVISION operator: these are again of two types float and floor division
+#float:The quotient returned by this operator is always a float number, no matter if two numbers are integers. 
+print(5/5)
+print(10/2)
+print(-10/2)
+print(20.0/2,"\n")
+
+#floor:The quotient returned by this operator is dependent on the argument being passed.
+print(10//3)
+print (-5//2)
+print (5.0//2)
+print (-5.0//2,"\n")
+
+#MODULUS operator:It is used to find the remainder when the first operand is divided by the second. 
+val1 = 3
+val2 = 2
+
+# using the modulus operator
+res = val1 % val2
+print(res,"\n")
+
+#Exponentiation Operator:It is used to raise the first operand to the power of the second. 
+val1 = 2
+val2 = 3
+
+# using the exponentiation operator
+res = val1 ** val2
+print(res,"\n")
+
+
+
+
+
+
